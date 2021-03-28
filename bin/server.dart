9034e5611c82256f67b60d6726fe42471aa264cc
Makuza Mugabo Verite <53856673+makuzaverite@users.noreply.github.com>
2021-03-28 20:52:57 +0200

    feat(fix): added tests packages

--- bin/server.dart
+++ bin/server.dart
@@ -3,7 +3,20 @@ import 'dart:io';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 
-// For Google Cloud Run, set _hostname to '0.0.0.0'.
 const _hostname = 'localhost';
+const _port = 8080;
 
-void main(List<String> args) async {}
+void main(List<String> args) async {
+  final handler = const shelf.Pipeline()
+      .addMiddleware(shelf.logRequests())
+      .addMiddleware((innerHandler) => (request) async {
+            final updatedRequest = request
+                .change(headers: {'custom header': 'custom header here'});
+            return shelf.Response.ok('intercepted');
+          })
+      .addHandler((request) => shelf.Response.ok("Hello world"));
+
+  final server = await io.serve(handler, _hostname, _port);
+
+  print('serving at http://${server.address.host}:${server.port}');
+}
