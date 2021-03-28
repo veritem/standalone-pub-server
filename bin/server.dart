13effe5de064a5e8b22947e79d8d25c5feeded69
Makuza Mugabo Verite <53856673+makuzaverite@users.noreply.github.com>
2021-03-28 20:14:03 +0200

    initial commit

---
+++ bin/server.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
+
+// For Google Cloud Run, set _hostname to '0.0.0.0'.
+const _hostname = 'localhost';
+
+void main(List<String> args) async {}
